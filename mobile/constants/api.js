7357ab79572ea8fd5a46cb11f0f7084ed8114f9d
Ishoula <shoulamite2k@gmail.com>
2025-11-22 12:32:53 +0200

    added local backend api

--- mobile/constants/api.js
+++ mobile/constants/api.js
@@ -1,2 +1,2 @@
-export const API_URL="https://mula-spln.onrender.com"
-// export const API_URL="http://localhost:2701/api"
+// export const API_URL="https://mula-spln.onrender.com"
+export const API_URL="http://localhost:2701/api"
